0b3ac0d3a63af4ef020e7c19214e6c7abd84022f
Lokesh R <lokesh21031999@gmail.com>
2024-09-25 14:26:35 +0530

    install and create the project

---
+++ api/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+const __dirname = path.resolve();
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000!");
+});
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
