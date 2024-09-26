5e296006dd71feb274b281d1d10e9045609de9d6
Lokesh R <lokesh21031999@gmail.com>
2024-09-26 11:51:13 +0530

    navbar

--- client/src/components/NavbarComp.jsx
+++ client/src/components/NavbarComp.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+/* eslint-disable react/prop-types */
+import React, { useState, useEffect } from "react";
 import { Navbar } from "flowbite-react";
 import { useLocation } from "react-router-dom";
 
-// eslint-disable-next-line react/prop-types
 const NavbarComp = ({ onToggle, isOpen }) => {
   const location = useLocation(); // Get current route
+  const [activeTable, setActiveTable] = useState("StudentTable"); // Default to StudentTable
+
+  useEffect(() => {
+    // Update the activeTable based on the current route
+    switch (location.pathname) {
+      case "/employee":
+        setActiveTable("EmployeeTable");
+        break;
+      case "/agriculture":
+        setActiveTable("AgricultureTable");
+        break;
+      default:
+        setActiveTable("StudentTable");
+    }
+  }, [location.pathname]);
 
   const getActiveClass = (path) => {
     return location.pathname === path ? "text-blue-600 underline" : "";
@@ -12,9 +27,9 @@ const NavbarComp = ({ onToggle, isOpen }) => {
 
   return (
     <Navbar fluid rounded>
-      <Navbar.Brand href="">
-        <span className="self-center whitespace-nowrap text-xl font-semibold ">
-          Logo
+      <Navbar.Brand href="/">
+        <span className="self-center whitespace-nowrap text-xl font-semibold cursor-pointer hover:text-blue-500">
+          {activeTable} {/* Display the active table name */}
         </span>
       </Navbar.Brand>
       <div className="flex md:order-2">
@@ -26,18 +41,21 @@ const NavbarComp = ({ onToggle, isOpen }) => {
         <Navbar.Link
           href="/"
           className={`hover:text-blue-500 ${getActiveClass("/")}`}
+          onClick={() => setActiveTable("StudentTable")}
         >
           StudentTable
         </Navbar.Link>
         <Navbar.Link
           href="/employee"
           className={`hover:text-blue-500 ${getActiveClass("/employee")}`}
+          onClick={() => setActiveTable("EmployeeTable")}
         >
           EmployeeTable
         </Navbar.Link>
         <Navbar.Link
           href="/agriculture"
           className={`hover:text-blue-500 ${getActiveClass("/agriculture")}`}
+          onClick={() => setActiveTable("AgricultureTable")}
         >
           AgricultureTable
         </Navbar.Link>
